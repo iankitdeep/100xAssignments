c0720e5c4281ab73123f3a9c930ceece2644589b
Ankit Deep <AnkitDeep01@outlook.com>
2025-11-13 05:40:44 +0000

    modified files

--- week-4/middlewares/01-requestcount.js
+++ week-4/middlewares/01-requestcount.js
@@ -1,6 +1,5 @@
 // You have to create a middleware for logging the number of requests on a server
-
-const express = require('express');
+const express = require("express");
 
 const app = express();
 let requestCount = 0;
@@ -10,16 +9,27 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+// function to keep track of no. of requests send to the server
+function noOfRequests(req, res, next) {
+  requestCount++;
+  next();
+}
+
+//  app.use() is used to mount global middleware i.e noOfRequests in this case
+app.use(noOfRequests);
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
 });
 
+// app.listen(3500);
 module.exports = app;
+// Done
